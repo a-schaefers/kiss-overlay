0196cf35020048357ff9d5bd6c3723776fc01b66
Adam Schaefers <paxchristi888@gmail.com>
2020-01-07 04:34:39 -0800

    commentary

--- boot.el/files/boot.el
+++ boot.el/files/boot.el
@@ -1,12 +1,20 @@
 #!/bin/dash
-":"; exec emacs --quick --script "$0" "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
+":"; exec /boot/emacs/bin/emacs --quick --script "$0" "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
 
-;; This was helpful:
-;; https://gist.github.com/lunaryorn/91a7734a8c1d93a8d1b0d3f85fe18b1e
+;; This was helpful - https://gist.github.com/lunaryorn/91a7734a8c1d93a8d1b0d3f85fe18b1e
 
 (setenv "PATH" "/bin")
 (setq exec-path '("/bin"))
-
 (setq debug-on-error t)
 
+;;HAAAACK
+
+
+
+
+
+
+
+
+;; Richard Stallman --out o/
 (kill-emacs 0)
