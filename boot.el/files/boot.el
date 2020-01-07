d878e4b2a128e2a9c1af9e283c7ff03e44b18203
Adam Schaefers <paxchristi888@gmail.com>
2020-01-07 04:13:03 -0800

    add boot.el file

---
+++ boot.el/files/boot.el
@@ -0,0 +1,12 @@
+#!/bin/dash
+":"; exec emacs --quick --script "$0" "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
+
+;; This was helpful:
+;; https://gist.github.com/lunaryorn/91a7734a8c1d93a8d1b0d3f85fe18b1e
+
+(setenv "PATH" "/bin")
+(setq exec-path '("/bin"))
+
+(setq debug-on-error t)
+
+(kill-emacs 0)
