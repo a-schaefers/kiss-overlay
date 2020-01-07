44b2146b2c27878118d752996f32528312f2b3cc
Adam Schaefers <paxchristi888@gmail.com>
2020-01-07 04:10:43 -0800

    add boot.el framework

--- sinit/files/config.h
+++
@@ -1,3 +0,0 @@
-static char *const rcinitcmd[]     = { "/lib/sinit/rc.boot", NULL };
-static char *const rcrebootcmd[]   = { "/lib/sinit/rc.shutdown", "reboot", NULL };
-static char *const rcpoweroffcmd[] = { "/lib/sinit/rc.shutdown", "poweroff", NULL };
